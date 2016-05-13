c52d9e344c3b5dd86ee9665505f002d5c573c019
wangw <wwwtete163.com>
2016-05-13 14:01:42 +0800

    修改UserInfo解析字段

--- app/src/main/java/com/wangw/goodhelp/model/UserInfo.java
+++ app/src/main/java/com/wangw/goodhelp/model/UserInfo.java
@@ -26,19 +26,18 @@ public class UserInfo {
      */
 
     private String user_id;
-    private String user_tag_id;
-    private String username;
-    private String mobile;
-    private String email;
-    private String passwd;
-    private String avatar_id;
-    private String like_count;
-    private String follower_count;
-    private String following_count;
-    private String rating;
-    private String locked;
-    private String created_time;
-    private List<String> tag;
+//    private String user_tag_id;
+//    private String username;
+//    private String mobile;
+//    private String email;
+//    private String passwd;
+//    private String avatar_id;
+//    private String like_count;
+//    private String follower_count;
+//    private String following_count;
+//    private String rating;
+//    private String locked;
+//    private String created_time;
 
     public String getUser_id() {
         return user_id;
@@ -48,107 +47,99 @@ public class UserInfo {
         this.user_id = user_id;
     }
 
-    public String getUser_tag_id() {
-        return user_tag_id;
-    }
-
-    public void setUser_tag_id(String user_tag_id) {
-        this.user_tag_id = user_tag_id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPasswd() {
-        return passwd;
-    }
-
-    public void setPasswd(String passwd) {
-        this.passwd = passwd;
-    }
-
-    public String getAvatar_id() {
-        return avatar_id;
-    }
-
-    public void setAvatar_id(String avatar_id) {
-        this.avatar_id = avatar_id;
-    }
-
-    public String getLike_count() {
-        return like_count;
-    }
-
-    public void setLike_count(String like_count) {
-        this.like_count = like_count;
-    }
-
-    public String getFollower_count() {
-        return follower_count;
-    }
-
-    public void setFollower_count(String follower_count) {
-        this.follower_count = follower_count;
-    }
-
-    public String getFollowing_count() {
-        return following_count;
-    }
-
-    public void setFollowing_count(String following_count) {
-        this.following_count = following_count;
-    }
-
-    public String getRating() {
-        return rating;
-    }
-
-    public void setRating(String rating) {
-        this.rating = rating;
-    }
-
-    public String getLocked() {
-        return locked;
-    }
-
-    public void setLocked(String locked) {
-        this.locked = locked;
-    }
-
-    public String getCreated_time() {
-        return created_time;
-    }
-
-    public void setCreated_time(String created_time) {
-        this.created_time = created_time;
-    }
-
-    public List<String> getTag() {
-        return tag;
-    }
-
-    public void setTag(List<String> tag) {
-        this.tag = tag;
-    }
+//    public String getUser_tag_id() {
+//        return user_tag_id;
+//    }
+//
+//    public void setUser_tag_id(String user_tag_id) {
+//        this.user_tag_id = user_tag_id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getMobile() {
+//        return mobile;
+//    }
+//
+//    public void setMobile(String mobile) {
+//        this.mobile = mobile;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPasswd() {
+//        return passwd;
+//    }
+//
+//    public void setPasswd(String passwd) {
+//        this.passwd = passwd;
+//    }
+//
+//    public String getAvatar_id() {
+//        return avatar_id;
+//    }
+//
+//    public void setAvatar_id(String avatar_id) {
+//        this.avatar_id = avatar_id;
+//    }
+//
+//    public String getLike_count() {
+//        return like_count;
+//    }
+//
+//    public void setLike_count(String like_count) {
+//        this.like_count = like_count;
+//    }
+//
+//    public String getFollower_count() {
+//        return follower_count;
+//    }
+//
+//    public void setFollower_count(String follower_count) {
+//        this.follower_count = follower_count;
+//    }
+//
+//    public String getFollowing_count() {
+//        return following_count;
+//    }
+//
+//    public void setFollowing_count(String following_count) {
+//        this.following_count = following_count;
+//    }
+//
+//    public String getRating() {
+//        return rating;
+//    }
+//
+//    public void setRating(String rating) {
+//        this.rating = rating;
+//    }
+//
+//    public String getLocked() {
+//        return locked;
+//    }
+//
+//    public void setLocked(String locked) {
+//        this.locked = locked;
+//    }
+//
+//    public String getCreated_time() {
+//        return created_time;
+//    }
+//
+//    public void setCreated_time(String created_time) {
+//        this.created_time = created_time;
+//    }
 }
